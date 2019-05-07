ff8a00bf4ad4ab5b1f2f95181d10512e789b95d0
wenzi712 <wdmeimei33@163.com>
2019-05-07 10:19:52 +0800

    kst

--- src/com/yhsem/kst/MsgEvent.java
+++ src/com/yhsem/kst/MsgEvent.java
@@ -51,6 +51,60 @@ public class MsgEvent {
     }
 
     public static String receivingData(HttpServletRequest request, HttpServletResponse response) {
+        Delegator delegator = (Delegator) request.getAttribute("delegator");
+
+        Map<String, String[]> paraMap = request.getParameterMap();
+        if (UtilValidate.isNotEmpty(paraMap) && UtilValidate.isNotEmpty(paraMap.get("data"))) {
+
+            String[] dataArray = paraMap.get("data");
+            JSONObject info = null;
+            JSONObject card = null;
+            for (int i = 0; i < dataArray.length; i++) {
+                Debug.logError("*********" + i + "*********", module);
+                Debug.logError("***" + dataArray[i], module);
+                Debug.logError("*********" + i + "*********", module);
+                Object tmp = JSON.parse(dataArray[i]);
+                JSONObject t = (JSONObject) tmp;
+                if (t.containsKey("recId")) {
+                    info = t;
+                } else if (t.containsKey("visitorId")) {
+                    card = t;
+                }
+
+            }
+
+            boolean beganTransaction = false;
+            try {
+                beganTransaction = TransactionUtil.begin();
+                boolean r = processingData(delegator, info, card);
+                if (r) {
+                    request.setAttribute("message", "ok");
+                    return "success";
+                } else {
+                    request.setAttribute("message", "error");
+                    return "error";
+                }
+            } catch (GenericEntityException e) {
+                try {
+                    TransactionUtil.rollback(beganTransaction, e.getLocalizedMessage(), e);
+                } catch (GenericEntityException e2) {
+                    Debug.logError(e2, "Could not rollback transaction: " + e2.toString(), module);
+                }
+                request.setAttribute("message", "error");
+                return "error";
+            } finally {
+                try {
+                    TransactionUtil.commit(beganTransaction);
+                } catch (GenericEntityException e) {
+                    Debug.logError(e, e.getLocalizedMessage(), module);
+                }
+            }
+        }
+        request.setAttribute("message", "ok");
+        return "success";
+    }
+
+    public static String _receivingData(HttpServletRequest request, HttpServletResponse response) {
         LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
         GenericValue userLogin = (GenericValue) request.getSession().getAttribute("userLogin");
         Delegator delegator = (Delegator) request.getAttribute("delegator");
@@ -63,17 +117,26 @@ public class MsgEvent {
                 JSONArray ja = (JSONArray) parse;
                 // TODO: 使用事务控制进行数据操作
                 // start
-                boolean r = true;
                 boolean beganTransaction = false;
                 try {
                     beganTransaction = TransactionUtil.begin();
-                    r = processingAllData(delegator, ja);
+                    boolean r = processingAllData(delegator, ja);
+
+                    if (r) {
+                        request.setAttribute("message", "ok");
+                        return "success";
+                    } else {
+                        request.setAttribute("message", "error");
+                        return "error";
+                    }
                 } catch (GenericEntityException e) {
                     try {
                         TransactionUtil.rollback(beganTransaction, e.getLocalizedMessage(), e);
                     } catch (GenericEntityException e2) {
                         Debug.logError(e2, "Could not rollback transaction: " + e2.toString(), module);
                     }
+                    request.setAttribute("message", "error");
+                    return "error";
                 } finally {
                     try {
                         TransactionUtil.commit(beganTransaction);
@@ -82,13 +145,6 @@ public class MsgEvent {
                     }
                 }
 
-                if (r) {
-                    request.setAttribute("message", "ok");
-                    return "success";
-                } else {
-                    request.setAttribute("message", "ok");
-                    return "success";
-                }
                 // end
             } else {
                 Debug.logError("parse receivingData Error", module);
@@ -136,8 +192,34 @@ public class MsgEvent {
     public static boolean processingData(Delegator delegator, JSONObject jo) {
         JSONObject visitorInfo = jo.getJSONObject("visitorInfo");
         JSONObject visitorCard = jo.getJSONObject("visitorCard");
+
+        if (visitorInfo.isEmpty() && visitorCard.isEmpty()) {
+            return true;
+        }
+
         boolean r1 = processingVisitorInfoData(delegator, visitorInfo);
         boolean r2 = processingVisitorCardData(delegator, visitorCard);
+        // 只有处理失败才返回错误
+        if (r1 && r2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean processingData(Delegator delegator, JSONObject info, JSONObject card) {
+        if (info.isEmpty() && card.isEmpty()) {
+            return true;
+        }
+
+        boolean r1 = true;
+        if (info != null && !info.isEmpty()) {
+            r1 = processingVisitorInfoData(delegator, info);
+        }
+        boolean r2 = true;
+        if (card != null && !card.isEmpty()) {
+            r2 = processingVisitorCardData(delegator, card);
+        }
         // 只有处理失败才返回错误
         if (r1 && r2) {
             return true;
@@ -155,22 +237,24 @@ public class MsgEvent {
      * @return
      */
     public static boolean processingVisitorInfoData(Delegator delegator, JSONObject visitorInfo) {
-        String recId = UtilValidate.isNotEmpty(visitorInfo.getString("recId")) ? visitorInfo.getString("recId") : null;// 主键
-        Debug.logError("recId : " + recId, module);
+        if (!visitorInfo.isEmpty()) {
+            String recId = UtilValidate.isNotEmpty(visitorInfo.getString("recId")) ? visitorInfo.getString("recId")
+                    : null;// 主键
+            Debug.logError("recId : " + recId, module);
 
-        GenericValue infoGv = null;
-        try {
-            infoGv = delegator.findOne("SemVisitorInfo", false, UtilMisc.toMap("recId", "recId"));
-            if (UtilValidate.isEmpty(infoGv)) {
-                infoGv = delegator.makeValue("SemVisitorInfo", UtilMisc.toMap("recId", "recId"));
-
+            GenericValue infoGv = null;
+            try {
+                infoGv = delegator.findOne("SemVisitorInfo", false, UtilMisc.toMap("recId", recId));
+                if (UtilValidate.isEmpty(infoGv)) {
+                    infoGv = delegator.makeValue("SemVisitorInfo", UtilMisc.toMap("recId", recId));
+                }
                 String visitorId = UtilValidate.isNotEmpty(visitorInfo.getString("visitorId")) ? visitorInfo
                         .getString("visitorId") : null;
                 String visitorName = UtilValidate.isNotEmpty(visitorInfo.getString("visitorName")) ? visitorInfo
                         .getString("visitorName") : null;
                 Date curEnterTime = UtilValidate.isNotEmpty(visitorInfo.getSqlDate("curEnterTime")) ? visitorInfo
                         .getSqlDate("curEnterTime") : null;
-                int curStayTime = UtilValidate.isNotEmpty(visitorInfo.getInteger("curStayTime")) ? visitorInfo
+                Integer curStayTime = UtilValidate.isNotEmpty(visitorInfo.getInteger("curStayTime")) ? visitorInfo
                         .getInteger("curStayTime") : 0;
                 String sourceIp = UtilValidate.isNotEmpty(visitorInfo.getString("sourceIp")) ? visitorInfo
                         .getString("sourceIp") : null;
@@ -188,9 +272,9 @@ public class MsgEvent {
                         .getSqlDate("diaEndTime") : null;
                 String terminalType = UtilValidate.isNotEmpty(visitorInfo.getString("terminalType")) ? visitorInfo
                         .getString("terminalType") : null;
-                int visitorSendNum = UtilValidate.isNotEmpty(visitorInfo.getInteger("visitorSendNum")) ? visitorInfo
+                Integer visitorSendNum = UtilValidate.isNotEmpty(visitorInfo.getInteger("visitorSendNum")) ? visitorInfo
                         .getInteger("visitorSendNum") : 0;
-                int csSendNum = UtilValidate.isNotEmpty(visitorInfo.getInteger("csSendNum")) ? visitorInfo
+                Integer csSendNum = UtilValidate.isNotEmpty(visitorInfo.getInteger("csSendNum")) ? visitorInfo
                         .getInteger("csSendNum") : 0;
                 String sourceUrl = UtilValidate.isNotEmpty(visitorInfo.getString("sourceUrl")) ? visitorInfo
                         .getString("sourceUrl") : null;
@@ -210,15 +294,15 @@ public class MsgEvent {
                         .getSqlDate("firstVisitTime") : null;
                 Date preVisitTime = UtilValidate.isNotEmpty(visitorInfo.getSqlDate("preVisitTime")) ? visitorInfo
                         .getSqlDate("preVisitTime") : null;
-                int totalVisitTime = UtilValidate.isNotEmpty(visitorInfo.getInteger("totalVisitTime")) ? visitorInfo
+                Integer totalVisitTime = UtilValidate.isNotEmpty(visitorInfo.getInteger("totalVisitTime")) ? visitorInfo
                         .getInteger("totalVisitTime") : 0;
                 String diaPage = UtilValidate.isNotEmpty(visitorInfo.getString("diaPage")) ? visitorInfo
                         .getString("diaPage") : null;
                 String curFirstViewPage = UtilValidate.isNotEmpty(visitorInfo.getString("curFirstViewPage")) ? visitorInfo
                         .getString("curFirstViewPage") : null;
-                int curVisitorPages = UtilValidate.isNotEmpty(visitorInfo.getInteger("curVisitorPages")) ? visitorInfo
+                Integer curVisitorPages = UtilValidate.isNotEmpty(visitorInfo.getInteger("curVisitorPages")) ? visitorInfo
                         .getInteger("curVisitorPages") : 0;
-                int preVisitPages = UtilValidate.isNotEmpty(visitorInfo.getInteger("preVisitPages")) ? visitorInfo
+                Integer preVisitPages = UtilValidate.isNotEmpty(visitorInfo.getInteger("preVisitPages")) ? visitorInfo
                         .getInteger("preVisitPages") : 0;
                 String operatingSystem = UtilValidate.isNotEmpty(visitorInfo.getString("operatingSystem")) ? visitorInfo
                         .getString("operatingSystem") : null;
@@ -228,21 +312,52 @@ public class MsgEvent {
                         : null;
                 String siteName = UtilValidate.isNotEmpty(visitorInfo.getString("siteName")) ? visitorInfo
                         .getString("siteName") : null;
-                int siteId = UtilValidate.isNotEmpty(visitorInfo.getInteger("siteId")) ? visitorInfo
-                        .getInteger("siteId") : 0;
+                String siteId = UtilValidate.isNotEmpty(visitorInfo.getString("siteId")) ? visitorInfo
+                        .getString("siteId") : null;
 
                 // 不处理对话数据
                 // JSONArray dialogs = visitorInfo.getJSONArray("dialogs");
                 infoGv.setString("visitorId", visitorId);
+                infoGv.setString("visitorName", visitorName);
+                infoGv.set("curEnterTime", curEnterTime);
+                infoGv.set("curStayTime", curStayTime.longValue());
+                infoGv.setString("sourceIp", sourceIp);
+                infoGv.setString("sourceProvince", sourceProvince);
+                infoGv.setString("sourceIpInfo", sourceIpInfo);
+                infoGv.setString("requestType", requestType);
+                infoGv.setString("endType", endType);
+                infoGv.set("diaStartTime", diaStartTime);
+                infoGv.set("diaEndTime", diaEndTime);
+                infoGv.setString("terminalType", terminalType);
+                infoGv.set("visitorSendNum", visitorSendNum.longValue());
+                infoGv.set("csSendNum", csSendNum.longValue());
+                infoGv.setString("sourceUrl", sourceUrl);
+                infoGv.setString("sourceType", sourceType);
+                infoGv.setString("searchEngine", searchEngine);
+                infoGv.setString("keyword", keyword);
+                infoGv.setString("firstCsId", firstCsId);
+                infoGv.setString("joinCsIds", joinCsIds);
+                infoGv.setString("dialogType", dialogType);
+                infoGv.set("firstVisitTime", firstVisitTime);
+                infoGv.set("preVisitTime", preVisitTime);
+                infoGv.set("totalVisitTime", totalVisitTime.longValue());
+                infoGv.setString("diaPage", diaPage);
+                infoGv.setString("curFirstViewPage", curFirstViewPage);
+                infoGv.set("curVisitorPages", curVisitorPages.longValue());
+                infoGv.set("preVisitPages", preVisitPages.longValue());
+                infoGv.setString("operatingSystem", operatingSystem);
+                infoGv.setString("browser", browser);
+                infoGv.setString("info", info);
+                infoGv.setString("siteName", siteName);
+                infoGv.setString("siteId", siteId);
 
-                delegator.create(infoGv);
+                delegator.createOrStore(infoGv);
+            } catch (GenericEntityException e) {
+                // TODO 自动生成的 catch 块
+                e.printStackTrace();
+                return false;
             }
-        } catch (GenericEntityException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-            return false;
         }
-
         // 只有处理失败才返回错误
 
         return true;
@@ -257,15 +372,17 @@ public class MsgEvent {
      * @return
      */
     public static boolean processingVisitorCardData(Delegator delegator, JSONObject visitorCard) {
-        String visitorId = UtilValidate.isNotEmpty(visitorCard.getString("visitorId")) ? visitorCard
-                .getString("visitorId") : null;
-        Debug.logError("visitorId : " + visitorId, module);
+        if (!visitorCard.isEmpty()) {
+            String visitorId = UtilValidate.isNotEmpty(visitorCard.getString("visitorId")) ? visitorCard
+                    .getString("visitorId") : null;
+            Debug.logError("visitorId : " + visitorId, module);
 
-        GenericValue cardGv = null;
-        try {
-            cardGv = delegator.findOne("SemVisitorCard", false, UtilMisc.toMap("visitorId", "visitorId"));
-            if (UtilValidate.isEmpty(cardGv)) {
-                cardGv = delegator.makeValue("SemVisitorCard", UtilMisc.toMap("visitorId", "visitorId"));
+            GenericValue cardGv = null;
+            try {
+                cardGv = delegator.findOne("SemVisitorCard", false, UtilMisc.toMap("visitorId", visitorId));
+                if (UtilValidate.isEmpty(cardGv)) {
+                    cardGv = delegator.makeValue("SemVisitorCard", UtilMisc.toMap("visitorId", visitorId));
+                }
                 String yongHeUserId = UtilValidate.isNotEmpty(visitorCard.getString("yongHeUserID")) ? visitorCard
                         .getString("yongHeUserID") : null;
 
@@ -326,14 +443,44 @@ public class MsgEvent {
                         .getSqlDate("lastChangeTime") : null;
 
                 cardGv.setString("yongHeUserId", yongHeUserId);
-                delegator.create(cardGv);
-            }
-        } catch (GenericEntityException e) {
-            // TODO 自动生成的 catch 块
-            e.printStackTrace();
-            return false;
-        }
+                cardGv.setString("visitorId", visitorId);
+                cardGv.setString("linkman", linkman);
+                cardGv.setString("cusType", cusType);
+                cardGv.setString("compName", compName);
+                cardGv.setString("webUrl", webUrl);
+                cardGv.setString("mobile", mobile);
+                cardGv.setString("phone", phone);
+                cardGv.setString("qq", qq);
+                cardGv.setString("msn", msn);
+                cardGv.setString("email", email);
+                cardGv.setString("address", address);
+                cardGv.setString("birthday", birthday);
+                cardGv.setString("channelType", channelType);
+                cardGv.setString("remark", remark);
+                cardGv.setString("loginName", loginName);
+                cardGv.setString("userName", userName);
+                cardGv.setString("nickName", nickName);
 
+                cardGv.set("addtime", addtime);
+                cardGv.set("lastChangeTime", lastChangeTime);
+
+                cardGv.setString("col1", col1);
+                cardGv.setString("col2", col2);
+                cardGv.setString("col3", col3);
+                cardGv.setString("col4", col4);
+                cardGv.setString("col5", col5);
+                cardGv.setString("col6", col6);
+                cardGv.setString("col7", col7);
+                cardGv.setString("col8", col8);
+                cardGv.setString("col9", col9);
+
+                delegator.createOrStore(cardGv);
+            } catch (GenericEntityException e) {
+                // TODO 自动生成的 catch 块
+                e.printStackTrace();
+                return false;
+            }
+        }
         // 只有处理失败才返回错误
 
         return true;
